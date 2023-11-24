c69ca527831d93ab0ed58db697ee9d63f6bc0c28
John Van Dalsem <vandalsemjj@gmail.com>
2023-11-24 13:27:13 -0800

    requirements didn't update

--- semiHeatMap.py
+++ semiHeatMap.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template
 import pandas as pd
 import numpy as np
+import requests
+from bs4 import BeautifulSoup as bs
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     companies = get_data()
+    sox_quote = get_sox()
     columns = 'Symbol,Name,Ctry,Primary Chip,Market Cap,EV/EBITDA (LTM),Total Return (YTD),Revenues (LTM),FCF (LTM),FCF/Share (LTM),P/E (LTM),PEG (NTM)'.split(',')
-    return render_template('index.html', companies=companies,columns=columns)
+    return render_template('index.html', companies=companies,columns=columns,sox_quote=sox_quote)
 
 # Helpers
 def get_data():
@@ -31,8 +34,25 @@ def get_data():
 
     companies['ISO Earnings'] = pd.to_datetime(companies['Next Earnings (Date)']).dt.strftime('%Y%m%d')
 
+    def total_return(row):
+        if row['Total Return (YTD)'] == '':
+            return ''
+        else:
+            return round((float(row['Total Return (YTD)'])*100),2)
+    
+    companies['Total Return (YTD)'] = companies.apply(total_return,axis=1)
+
     companies = companies.to_dict('records')
     return companies
+
+def get_sox():
+    page = requests.get("https://finance.yahoo.com/quote/%5ESOX?p=^SOX&.tsrc=fin-srch",headers={'User-Agent': 'Mozilla/5.0'})
+    soup = bs(page.text,'html.parser')
+    quote = soup.find_all("div", {"class": "D(ib) Mend(20px)"})
+    price = quote[0].find('fin-streamer').get("value")
+    prince_change = quote[0].find_all('span')[0].text
+    perc_change = quote[0].find_all('span')[1].text[1:-1]
+    return [price,prince_change,perc_change]
 
 if __name__ == '__main__':
     app.run(debug=True)
